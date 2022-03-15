92b8b99805dbca8e097040be29d6dddc521a3e8c
Shazron Abdullah <shazron@gmail.com>
2022-03-15 11:36:45 +0800

    fix: swap isomorphic-fetch for cross-fetch for security issue
    See #95

--- src/invoker.js
+++ src/invoker.js
@@ -17,7 +17,7 @@
 
 'use strict';
 
-const fetch = require('fetch-retry')(require('isomorphic-fetch'));
+const fetch = require('fetch-retry')(require('cross-fetch'));
 const kinds = require('./kinds/kinds');
 const path = require('path');
 const log = require("./log");
